cc63e27d49c4df9b7f16a0a0dbfba70fe62ace30
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-07-30 08:21:15 +0900

    073025

---
+++ scripts/todo-list.js
@@ -0,0 +1,30 @@
+    // Make and empty array
+const todoList = ['make dinner', 'wash dishes'];
+
+function renderTodoLisst () {
+    let todoListHTML = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`;
+        todoListHTML += html;
+    }
+
+    console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
+}
+    //function for addTodo in Add button
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+
+    const name = inputElement.value;
+
+    todoList.push(name);
+    console.log(todoList);
+
+    inputElement.value = '';
+
+    renderTodoLisst();
+}
